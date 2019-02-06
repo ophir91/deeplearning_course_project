37babef49a3442c7e2120083976eb891d32df584
opihr91 <opihryaniv@gmail.com>
2019-02-06 16:34:28 +0200

    TODO: write the dataloader

--- dataloader.py
+++ dataloader.py
@@ -2,7 +2,7 @@ from torch.utils.data import Dataset
 import torch
 
 
-class MyDataset(Dataset):
+class MyDataset(Dataset):  # TODO: need to write the data loader
     def __init__(self):
         pass
 
@@ -10,9 +10,9 @@ class MyDataset(Dataset):
         return 2000
 
     def __getitem__(self, item):
-        sample = {'mfcc': torch.rand(1,39*9), 'stft': torch.rand(1,257*9), 'ground_truth': torch.rand(1,257)}
 
-        return sample
+        sample = {'mfcc': torch.rand(1,39*9), 'stft': torch.rand(1,257*9), 'ground_truth': torch.rand(1,257)}
+        return sample  # the sample the will be return nee to be in the above format
         pass
 
 
